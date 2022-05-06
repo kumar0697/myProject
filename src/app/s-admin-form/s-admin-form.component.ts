704af78a6500784e6976d96fb3170f68aa993ac4
dharamkumar singh <singhdharamkumar7@gmail.com>
2022-05-06 20:03:06 +0530

    s_admin updated

--- src/app/s-admin-form/s-admin-form.component.ts
+++ src/app/s-admin-form/s-admin-form.component.ts
@@ -7,13 +7,30 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./s-admin-form.component.css']
 })
 export class SAdminFormComponent implements OnInit {
+ NgForm:any
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
   save(form:NgForm){
-    console.log(form);
+    let user = form.value;
+    
+    let usersJson :any = localStorage.getItem('user');
+     
+    let users = JSON.parse(usersJson);
+    
+    users = []
+    users.push( user )
+     ( users.length  | users.length )
+    
+
+
   }
+  
 
 }
+
+
+
