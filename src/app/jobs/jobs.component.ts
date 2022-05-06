9cfcb2b4aea8595e5705def7764ca7e0c8f39ae9
dharamkumar singh <singhdharamkumar7@gmail.com>
2022-05-06 18:40:38 +0530

    Create jobs component and  its form

---
+++ src/app/jobs/jobs.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.css']
+})
+export class JobsComponent implements OnInit {
+  NgForm: any
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  save(form: NgForm) {
+
+    let job = form.value;
+
+    let jobJson: any = localStorage.getItem('jobs')
+
+    let jobs = JSON.parse(jobJson)
+
+    if (!jobs) {
+      jobs = [];
+      jobs.push(job)
+      localStorage.setItem('jobs', JSON.stringify(jobs))
+    }
+    else {
+      jobs.push(job)
+      localStorage.setItem('jobs', JSON.stringify(jobs))
+    }
+    form.reset();
+
+
+  }
+
+}
